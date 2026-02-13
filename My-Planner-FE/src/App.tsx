fe7c82c50a66df1ff10db2adafab205a579e6279
pramuddhika <pramuddhika21@gmail.com>
2026-02-13 07:25:45 +0530

    folder structure fixed

--- My-Planner-FE/src/App.tsx
+++ My-Planner-FE/src/App.tsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-sky-950">Vite + React</h1>
+      <h1 className="text-primary text-3xl font-bold underline bg-sky-950">Vite + React</h1>
     </>
   )
 }
